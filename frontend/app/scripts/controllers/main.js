f15134fdb30ba895d26104d2824bc15fea03ab88
cheetah354 <adrianopaulus@adrians-macbook-pro.fritz.box>
2013-09-07 14:58:18 +0200

    Output von yeoman angular

---
+++ frontend/app/scripts/controllers/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('frontendApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+      'Apartmapp'
+    ];
+  });
