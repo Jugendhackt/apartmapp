7e9d4938d482738870e54e0d217cf594ea017133
Adrian P <cheetah354@web.de>
2013-09-08 11:33:56 +0200

    Login und Signup eingebaut

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -7,6 +7,14 @@ angular.module('frontendApp', [])
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
+      .when('/signup', {
+        templateUrl: 'views/signup.html',
+        controller: 'SignupCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -8,4 +8,21 @@ angular.module('frontendApp')
       'Karma',
       'Apartmapp'
     ];
+  })
+ .controller('LoginCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+      'Apartmapp'
+    ];
+  })
+ .controller('SignupCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+      'Apartmapp'
+    ];
   });
+
