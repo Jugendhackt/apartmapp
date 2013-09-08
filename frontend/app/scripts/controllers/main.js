174a39c6602f0dc9bb4ed7944f3ef6d4a5a2db92
radopi <me@me.com>
2013-09-08 11:49:21 +0200

    Including search results in main page

--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -2,10 +2,6 @@
 
 angular.module('frontendApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma',
-      'Apartmapp'
+    $scope.searchResults = [{city: "Eichsfeld (Kreis)", description: "3-R-Whg. mit Balkon, Laminat - mit Weitblick \u00fcbers Tal", preciseHouseNumber: "true", street: "Stra\u00dfe des Aufbaus", image_url: "http://picture.immobilienscout24.de/pic/orig04/N/1/51/455/1051455-0.jpg/ORIG/resize/60x60%3E/extent/60x60/format/jpg?3091713215", postcode: "37345", wgs84Coordinate: {latitude: 51.474296184543235, longitude: 10.487635936473437}, quarter: "Gro\u00dfbodungen", houseNumber: "14-18"}, {city: "Eichsfeld (Kreis)", description: "1-R-Whg. mit Balkon, Laminat - mit Weitblick \u00fcbers Tal", preciseHouseNumber: "true", street: "Stra\u00dfe des Aufbaus", image_url: "http://picture.immobilienscout24.de/pic/orig03/N/177/883/494/177883494-0.jpg/ORIG/resize/60x60%3E/extent/60x60/format/jpg?3091713215", postcode: "37345", wgs84Coordinate: {latitude: 51.473689813304084, longitude: 10.486402115693128}, quarter: "Gro\u00dfbodungen", houseNumber: "14-18"}
     ];
   });
