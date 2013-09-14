576f6480107b8babe6cc8dfd83aa2584f8c400d0
radopi <me@me.com>
2013-09-14 11:32:21 +0200

    Added Markers to Map

--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -2,10 +2,35 @@
 
 angular.module('frontendApp')
   .controller('MainCtrl', function ($scope, $http) {
+    $scope.center = {
+        lat: 51.5,
+        lng: 10.5,
+        zoom: 4
+    },
+    $scope.markers = []
     $scope.searchResults = [];
     $scope.error = false;
     $http.get('/-/search').success( function(data) {
       $scope.searchResults = data.results;
+      for (var i=0;i<$scope.searchResults.length;i++) {
+        console.log($scope.searchResults[i])
+        if ($scope.searchResults[i]['wgs84Coordinate'])
+        {
+          $scope.markers.push({
+            lat : $scope.searchResults[i]['wgs84Coordinate']['latitude'],
+            lng : $scope.searchResults[i]['wgs84Coordinate']['longitude'],
+          });
+        }
+        //var marker1 = L.marker([$scope.searchResults[i]["wgs84Coordinate"]['latitude'], $scope.searchResults[i]['wgs84Coordinate']['longitude']]).addTo(leaflet);
+        //var marker1 = L.marker([entry['wgs84Coordinate']['latitude'], entry['wgs84Coordinate']['longitude']]).addTo(map);
+      }
+    }
+    ).error(function() {
+      $scope.error = true;
+    });
+    
+    $http.get('/-/others').success( function(data) {
+      $scope.otherResults = data.results;
     }
     ).error(function() {
       $scope.error = true;
