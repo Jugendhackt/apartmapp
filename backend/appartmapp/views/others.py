82c6e9ee78bc97365024fe40d27273a66082adf2
radopi <me@me.com>
2013-09-14 16:20:09 +0200

    Markers are now clickable and show a desription

--- backend/appartmapp/views/others.py
+++ backend/appartmapp/views/others.py
@@ -5,10 +5,12 @@ search = Service(name='others', path=path('others'), renderer='json')
 
 @search.get(accept='application/json')
 def query(request):
+	firstLong = 51.5
+	firstLat = 10.5
 	con = psycopg2.connect(database='appartmapp', user='postgres') 
 	cur = con.cursor()
 	cur.execute('SELECT * from items')
 	rows = cur.fetchall()
-	results = [(row) for row in rows]
+	results = [(row) for row in rows if abs(float(row[4]) - firstLat)<10 and abs(float(row[5]) - firstLong)<20]
 	return dict(results=results)
 
--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -5,7 +5,7 @@ angular.module('frontendApp')
     $scope.center = {
         lat: 51.5,
         lng: 10.5,
-        zoom: 4
+        zoom: 5
     },
     $scope.markers = []
     $scope.searchResults = [];
@@ -19,6 +19,7 @@ angular.module('frontendApp')
           $scope.markers.push({
             lat : $scope.searchResults[i]['wgs84Coordinate']['latitude'],
             lng : $scope.searchResults[i]['wgs84Coordinate']['longitude'],
+            message : "<a href=http://www.immobilienscout24.de/expose/" + $scope.searchResults[i]["information"] + ">" + $scope.searchResults[i]['description'] + "</a>"
           });
         }
         //var marker1 = L.marker([$scope.searchResults[i]["wgs84Coordinate"]['latitude'], $scope.searchResults[i]['wgs84Coordinate']['longitude']]).addTo(leaflet);
@@ -31,6 +32,15 @@ angular.module('frontendApp')
     
     $http.get('/-/others').success( function(data) {
       $scope.otherResults = data.results;
+      for (var i=0;i<$scope.otherResults.length;i++) {
+        //console.log($scope.otherResults[i])
+        $scope.markers.push({
+          lat : $scope.otherResults[i][4],
+          lng : $scope.otherResults[i][5],
+        });
+        //var marker1 = L.marker([$scope.searchResults[i]["wgs84Coordinate"]['latitude'], $scope.searchResults[i]['wgs84Coordinate']['longitude']]).addTo(leaflet);
+        //var marker1 = L.marker([entry['wgs84Coordinate']['latitude'], entry['wgs84Coordinate']['longitude']]).addTo(map);
+      }
     }
     ).error(function() {
       $scope.error = true;
