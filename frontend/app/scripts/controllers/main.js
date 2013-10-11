5f3d4220dd9ade51f10564c55160ddfb47a4e43e
radopi <me@me.com>
2013-10-11 11:43:07 +0200

    Added logo onload and removed list of things next to the map

--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -19,7 +19,7 @@ angular.module('frontendApp')
           $scope.markers.push({
             lat : $scope.searchResults[i]['wgs84Coordinate']['latitude'],
             lng : $scope.searchResults[i]['wgs84Coordinate']['longitude'],
-            message : "<a href=http://www.immobilienscout24.de/expose/" + $scope.searchResults[i]["information"] + ">" + $scope.searchResults[i]['description'] + "</a>"
+            message : '<li><img src="' + $scope.searchResults[i]["image_url"] + '"><p></p>' + $scope.searchResults[i]["description"] + '<p>' + $scope.searchResults[i]["street"] + " " + $scope.searchResults[i]["houseNumber"] + ', ' + $scope.searchResults[i]["postcode"]  + " " + $scope.searchResults[i]["city"] + '</p><p><a href="http://www.immobilienscout24.de/expose/' + $scope.searchResults[i]["information"] + '"><img height=75 width=163 src="images/informationen.png"></a></p></li>'
           });
         }
         //var marker1 = L.marker([$scope.searchResults[i]["wgs84Coordinate"]['latitude'], $scope.searchResults[i]['wgs84Coordinate']['longitude']]).addTo(leaflet);
@@ -37,7 +37,15 @@ angular.module('frontendApp')
    $scope.markers.push({
           lat : $scope.otherResults[i][4],
           lng : $scope.otherResults[i][5],
-          message : $scope.otherResults[i][4] + $scope.otherResults[i][5]
+          message : $scope.otherResults[i][4] + $scope.otherResults[i][5],
+          icon: L.icon({
+                    iconUrl: 'images/' + $scope.otherResults[i][3],
+                    iconSize: [80, 80],
+                    iconAnchor: [40, 80],
+                    popupAnchor: [0, 0],
+                    shadowSize: [0, 0],
+                    shadowAnchor: [0, 0]
+                })
         });
         //var marker1 = L.marker([$scope.searchResults[i]["wgs84Coordinate"]['latitude'], $scope.searchResults[i]['wgs84Coordinate']['longitude']]).addTo(leaflet);
         //var marker1 = L.marker([entry['wgs84Coordinate']['latitude'], entry['wgs84Coordinate']['longitude']]).addTo(map);
@@ -47,10 +55,14 @@ angular.module('frontendApp')
       $scope.error = true;
     });
     $scope.$on('leafletDirectiveMap.zoomstart', function () { 
-        $scope.markers = []
+    $scope.markers = []
     $scope.searchResults = [];
+    $scope.otherResults = [];
     $scope.error = false;
-    /*$http.get('/-/search').success( function(data) {
+    $http.get('/-/search', {
+      params: {lng : $scope.center.lng,
+               lat : $scope.center.lat,
+               dist : 5000}}).success( function(data) {
       $scope.searchResults = data.results;
       for (var i=0;i<$scope.searchResults.length;i++) {
         //console.log($scope.searchResults[i])
@@ -59,7 +71,7 @@ angular.module('frontendApp')
           $scope.markers.push({
             lat : $scope.searchResults[i]['wgs84Coordinate']['latitude'],
             lng : $scope.searchResults[i]['wgs84Coordinate']['longitude'],
-            message : "<a href=http://www.immobilienscout24.de/expose/" + $scope.searchResults[i]["information"] + ">" + $scope.searchResults[i]['description'] + "</a>"
+            message : '<li><img src="' + $scope.searchResults[i]["image_url"] + '"><p></p>' + $scope.searchResults[i]["description"] + '<p>' + $scope.searchResults[i]["street"] + " " + $scope.searchResults[i]["houseNumber"] + ', ' + $scope.searchResults[i]["postcode"]  + " " + $scope.searchResults[i]["city"] + '</p><p><a href="http://www.immobilienscout24.de/expose/' + $scope.searchResults[i]["information"] + '"><img height=75 width=163 src="images/informationen.png"></a></p></li>'
           });
         }
         //var marker1 = L.marker([$scope.searchResults[i]["wgs84Coordinate"]['latitude'], $scope.searchResults[i]['wgs84Coordinate']['longitude']]).addTo(leaflet);
@@ -69,15 +81,26 @@ angular.module('frontendApp')
     ).error(function() {
       $scope.error = true;
     });
-    */
-    $http.get('/-/others').success( function(data) {
+    
+    $http.get('/-/others', {
+      params: {lng : $scope.center.lng,
+               lat : $scope.center.lat,
+               dist : 0.5}}).success( function(data) {
       $scope.otherResults = data.results;
       for (var i=0;i<$scope.otherResults.length;i++) {
-        //console.log($scope.otherResults[i])
+        console.log($scope.otherResults[i])
         $scope.markers.push({
           lat : $scope.otherResults[i][4],
           lng : $scope.otherResults[i][5],
-          message : $scope.otherResults[i][4] + $scope.otherResults[i][5]
+          message : $scope.otherResults[i][4] + $scope.otherResults[i][5],
+          icon: L.icon({
+                    iconUrl: 'images/' + $scope.otherResults[i][3],
+                    iconSize: [80, 80],
+                    iconAnchor: [40, 80],
+                    popupAnchor: [0, 0],
+                    shadowSize: [0, 0],
+                    shadowAnchor: [0, 0]
+                })
         });
         //var marker1 = L.marker([$scope.searchResults[i]["wgs84Coordinate"]['latitude'], $scope.searchResults[i]['wgs84Coordinate']['longitude']]).addTo(leaflet);
         //var marker1 = L.marker([entry['wgs84Coordinate']['latitude'], entry['wgs84Coordinate']['longitude']]).addTo(map);
